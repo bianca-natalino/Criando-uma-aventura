84201489befb48370646c845566455d27daf20ae
bianca-natalino <bianca.natalino.duarte@escola.pr.gov.br>
2025-11-18 08:19:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const avanca = document.querySelector('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca.forEach(button =>){
-    button.addEventListener('click', function){
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo' + this.getATRIbut('data-proxima');
-    }
-    atual.classList.remore(ativo);
-    document.getElementeyById(proximoPasso).classList.add('ativo');
-    
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
